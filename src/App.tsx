1792180e8b0aa55675632b0dc5f2d827b8f3ce43
Thein Naing <theinnaing0908@gmail.com>
2023-06-09 13:36:33 +0800

    import useState hook , create 3 useState hooks for task,deadline days,todo with string,number object like as below: const [task, setTask] = useState<string>('');   const [days, setDays] = useState<number>(0);   const [todo, setTodoList] = useState<string>([]);

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import './App.css';
 // import {TodoList} from './components/Todolist';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [days, setDays] = useState<number>(0);
+  const [todo, setTodoList] = useState<string>([]);
 
+const handleChange = (e) => {
 
+}
   return (
 
       <div className="App">
         <div className="header">
             <div className="input_form_container">
             <input
+            value={task}
+            onChange={handleChange}
             type='text' placeholder='Add your todo'/>
-            <input type='number'
+            <input
+            value={task}
+            onChange={handleChange}
+            type='number'
             placeholder='Need to finished( in days)'
             />
             </div>
               <button>Add</button>
         </div>
           {/* <button>Add</button> */}
-          {/* <div className="todolist"> */}
+          <div className="todoList">
       </div>
       // </div>
 
