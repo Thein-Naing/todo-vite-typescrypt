af8febf90435505cfd7246c99db1c0b1f50afceb
Thein Naing <theinnaing0908@gmail.com>
2023-06-09 15:12:13 +0800

    update App.tsx with compleTodo function and also new ts file TodoTask.ts

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { ITask } from "../Interfaces";
 
-const todoTask = () => {
-  return (
-    <div>todo</div>
-  )
+interface Props {
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
 }
 
-export default todoTask
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+export default TodoTask;
